c5aca1239833aeb904a91c96ab638ce6b6eaff94
Jacob Knox <jacobknoxa@gmail.com>
2023-09-08 13:52:03 -0400

    Navbar looking good and start footer

--- fsc-file-share/resources/views/components/footer.blade.php
+++ fsc-file-share/resources/views/components/footer.blade.php
@@ -1,3 +1,44 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+<div class="container">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+      <div class="col mb-3">
+        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+          <p>Jacob Knox</p>
+        </a>
+        <p class="text-muted">© 2023</p>
+      </div>
+  
+      <div class="col mb-3">
+        <h5>Site Map</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Files</a></li>
+            @if(Auth::check() || config('myconfig.testing'))
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Profile</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Settings</a></li>
+            @else
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Login</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Sign Up</a></li>
+            @endif
+        </ul>
+      </div>
+  
+      <div class="col mb-3">
+        <h5>Important</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms & Conditions</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
+        </ul>
+      </div>
+  
+      <div class="col mb-3">
+        <h5>Contact</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+    </footer>
 </div>
--- fsc-file-share/resources/views/components/header.blade.php
+++ fsc-file-share/resources/views/components/header.blade.php
@@ -1,44 +1,53 @@
 @php
-    $buttonStyles = "background-color: #0032A0; color: white; border: 2px black solid; border-radius: 10px; width: 80px; text-align: center;";
-    $buttonClasses = "nav-item nav-link mx-0 mx-md-1 my-1 my-md-0 col"
+    $buttonStyles = "background-color: #0032A0; color: white; border: 2px black solid; border-radius: 10px; text-align: center; min-width: 80px;";
+    $buttonClasses = "nav-item nav-link mx-0 mx-md-1 my-1 my-md-0 btn-block"
 @endphp
 
 <nav class="navbar sticky-top navbar-expand-md" style="background-color:#BA0C2F; color:white;">
-    <a class="navbar-brand" href="/">FSC File Share</a>
-    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <ul class="navbar-nav mx-auto">
+        <div class="nav-item">
+            <a class="navbar-brand mx-0 mr-md-2" href="/">FSC File Share</a>
+        </div>
+        <div class="nav-item text-center">
+            <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </ul>
+    
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <ul class="navbar-nav mr-auto text-center container">
-            <div class="row row-cols-1 @php echo(Auth::check() ? "row-cols-md-4" : "row-cols-md-2") @endphp">
-                <a class="{{$buttonClasses}}" href="/" style="{{$buttonStyles}}">Home</a>
-                <a class="{{$buttonClasses}}" href="#" style="{{$buttonStyles}}">Files</a>
-                <!-- If the user is logged in (authenticated), then display appropriate links -->
-                @if(Auth::check() || config('myconfig.testing'))
-                    <a class="{{$buttonClasses}}" href="#" style="{{$buttonStyles}}">Profile</a>
-                    <a class="{{$buttonClasses}}" href="#" style="{{$buttonStyles}}">Settings</a>
-                <!-- If they're not, then show the appropriate option to log in -->
-                @else
-                    <!-- Login button, hidden on screens medium or bigger -->
-                    <a class="{{$buttonClasses}} d-md-none" href="#" style="{{$buttonStyles}}">Login</a>
-                @endif
-            </div>
-            
+        <ul class="navbar-nav mr-auto">
+            <a class="{{$buttonClasses}}" href="/" style="{{$buttonStyles}}">Home</a>
+            <a class="{{$buttonClasses}}" href="#" style="{{$buttonStyles}}">Files</a>
+            <!-- If the user is logged in (authenticated), then display appropriate links -->
+            @if(Auth::check() || config('myconfig.testing'))
+                <a class="{{$buttonClasses}}" href="#" style="{{$buttonStyles}}">Profile</a>
+                <a class="{{$buttonClasses}}" href="#" style="{{$buttonStyles}}">Settings</a>
+                <span class="navbar-text text-center d-md-none">Welcome, user!</span>
+            <!-- If they're not, then show the appropriate option to log in -->
+            @else
+                <!-- Login button, hidden on screens medium or bigger -->
+                <a class="{{$buttonClasses}} d-md-none" href="#" style="{{$buttonStyles}}">Login</a>
+                <a class="{{$buttonClasses}} d-md-none" href="#" style="{{$buttonStyles}}">Sign Up</a>
+            @endif
         </ul>
         @if(Auth::check() || config('myconfig.testing'))
-            <span class="navbar-text">Welcome, user!</span>
+            <span class="navbar-text d-none d-md-block">Welcome, user!</span>
         @else
             <!-- Login form, hidden on screens smaller than medium (when burger pops up) -->
-            <form class="form-inline d-none d-md-block">
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">@</span>
+            <ul class="my-0">
+                <form class="form-inline d-none d-md-block">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="password" class="form-control" placeholder="Password" aria-label="Password">
+                        <button class="btn btn-success ml-md-2" type="submit">Login</button>
                     </div>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                    <input type="password" class="form-control" placeholder="Password" aria-label="Password">
-                    <button class="btn btn-outline-success" type="submit">Login</button>
-                </div>
-            </form>
+                </form>
+                <span class="navbar-text d-none d-md-block py-0 align-right" style="font-size: x-small;">Don't have an account? <a href="#" style="color:white; text-decoration: underline;">Create one!</a></span>
+            </ul>
         @endif
-    </div>
+    </div>  
 </nav>
