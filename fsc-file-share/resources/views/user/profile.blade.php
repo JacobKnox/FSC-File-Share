c118d3850012565d295fce8bc3b194941724b4d8
Jacob Knox <jacobknoxa@gmail.com>
2023-10-02 18:30:21 -0400

    Add some stuff to the profile page

--- fsc-file-share/resources/views/user/profile.blade.php
+++ fsc-file-share/resources/views/user/profile.blade.php
@@ -1,5 +1,61 @@
 <x-template pageName="{{$user->username}} Profile">
-    @if(Auth::user() == $user)
+    <p>{{$user->name}}</p>
+    <p>{{$user->username}}</p>
+    <p>Files Uploaded</p>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2">
+            @foreach($user->files as $file)
+                <div class="col align-self-center my-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-center"><a href="/files/{{$file->id}}" class="card-link">{{$file->title}}</a></h5>
+                            <h6 class="card-subtitle mb-2 text-muted text-capitalize">{{implode(', ', $file->tags())}}</h6>
+                            <p class="card-text">{{$file->description}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <p>Comments Left</p>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-2">
+            @foreach($user->comments as $comment)
+                <div class="col align-self-center my-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            @if($comment->file == null)
+                                <p>Deleted File</p>
+                            @else
+                                <h5 class="card-title text-center"><a href="/files/{{$comment->file->id}}" class="card-link">{{$comment->file->title}}</a></h5>
+                            @endif
+                            <p class="card-text">{{$comment->content}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    @if(Auth::user()->id == $user->id)
+        <p>Liked Files</p>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-2">
+                @foreach($user->likes as $like)
+                    <div class="col align-self-center my-3">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                @if($like->file == null)
+                                    <p>Deleted File</p>
+                                @else
+                                    <h5 class="card-title text-center"><a href="/files/{{$like->file->id}}" class="card-link">{{$like->file->title}}</a></h5>
+                                    <p class="card-text">{{$like->file->description}}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
         <form method="POST" action="/users/{{$user->id}}">
             @method('DELETE')
             @csrf
