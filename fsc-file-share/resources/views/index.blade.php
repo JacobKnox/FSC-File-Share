b44de2adbdbf61adbf56bdf05d81e7c91019bbeb
Jacob Knox <jacobknoxa@gmail.com>
2023-10-19 10:10:38 -0400

    Updating index page development info

--- fsc-file-share/resources/views/index.blade.php
+++ fsc-file-share/resources/views/index.blade.php
@@ -23,8 +23,11 @@
                         FSC File Share came to me one day as I was brainstorming ideas for my Honors senior project. I wanted something that would both apply the skills I already learned and challenge me to learn more. Then I thought about how I often like to share papers and projects I've done for my courses that I was proud of, which led me to the idea of building a website for that specific purpose.
                     </div>
                     <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
-                        <p class="h5">Pre-Alpha (September 4th, 2023 - Ongoing)</p>
-                        <p>Currently building the skeleton of the website. Will update as development progresses.</p>
+                        <p class="h5">Pre-Alpha (September 4th, 2023 - October 16th, 2023)</p>
+                        <p>Developed all features necessary for the Minimum Viable Product (MVP): user accounts, file system, comments and likes, etc.</p>
+                        <p class="h5">Alpha (October 16th, 2023 - Ongoing)</p>
+                        <p>Currently working on building on top of the MVP including some stretch goals, code cleaning, and security buffs.<br>
+                        {{-- <a href="https://github.com/JacobKnox/FSC-File-Share/releases/tag/v0.1.0-alpha">Alpha v0.1.0 Release Notes</a></p> --}}
                     </div>
                     <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
                         Hello, my name is Jacob Knox. I am expecting my Bachelor's of Science in Computer Science (AI and Web concentrations) and Applied Mathematics (STEM concentration) from Florida Southern College in May 2024. I have a data analtyics minor, and I am a part of the Honors program. Once I graduate, I am aiming to be a software engineer/developer.
